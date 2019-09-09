f7f5c5282a489c86d77120ac23a8f523433a6364
Andy <andy@mediasuite.co.nz>
2019-09-09 13:09:26 +1200

    First commit

---
+++ layouts/default.vue
@@ -0,0 +1,234 @@
+<template>
+  <div>
+    <div class="header">
+      <div class="title">
+        <h1><a href="/">Jones and Wilkinson LTD</a></h1>
+      </div>
+      <div class="contact-wrapper">
+        <div class="contact-details">
+          <a href="mailto:sales@jonesandwilkinson.co.uk">sales@jonesandwilkinson.co.uk</a>
+          <a href="tel:+44 1922 475444">+44 1922 475444</a>
+        </div>
+        <button>Get a quote</button>
+      </div>
+    </div>
+
+    <nuxt />
+
+    <div class="footer">
+      <div>
+        <h4>Jones&nbsp;&amp;&nbsp;Wilkinson&nbsp;LTD</h4>
+        <ul class="address">
+          <li>Ezekiel Lane</li>
+          <li>Willenhall</li>
+          <li>West Midlands</li>
+          <li>WV12 5QU</li>
+        </ul>
+      </div>
+      <div>
+        <h4>Policies</h4>
+        <ul>
+          <li><a href="#">Data Protection Policy</a></li>
+          <li><a href="#">Privacy Notice</a></li>
+          <li><a href="#">Anti slavery and human trafficking policy</a></li>
+          <li><a href="#">Terms and conditions</a></li>
+        </ul>
+      </div>
+      <div>
+        <h4>VAT no. 100235836</h4>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 62.5% !important;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*:before,
+*:after {
+  box-sizing: border-box;
+  margin: 0;
+  border-width: 0;
+  border-style: solid;
+  border-color: #e2e8f0;
+}
+
+h1 {
+  font-size: 2.2rem;
+}
+h2 {
+  font-size: 1.8rem;
+}
+p {
+  font-size: 0.9rem;
+  margin-bottom: 20px;
+}
+
+.header {
+  display: flex;
+  flex-direction: column;
+  vertical-align: middle;
+  align-content: space-between;
+  height: 80px;
+}
+.title {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 5px 0;
+}
+.title h1 {
+  margin: 0;
+}
+.title a {
+  margin: 0 30px;
+  display: flex;
+  align-items: center;
+  color: #1d2e7e;
+  text-decoration: none;
+}
+.contact-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+}
+.contact-wrapper button {
+  padding: 7px 16px;
+  background-color: #3c61a0;
+  color: white;
+  cursor: pointer;
+  font-weight: 600;
+  font-size: 1rem;
+}
+
+.footer {
+  background-color: #4e5d78;
+  padding: 20px 10px;
+  color: white;
+  display: flex;
+  font-size: 1rem;
+  justify-content: space-around;
+  position: fixed;
+  bottom: 0;
+}
+.footer .address li {
+  list-style: none;
+}
+.footer .address {
+  padding-left: 10px;
+}
+.footer h4 {
+  text-align: center;
+  margin-bottom: 10px;
+}
+.footer a {
+  color: white;
+  text-decoration: none;
+}
+.contact-details a {
+  margin: 0;
+  font-size: 0.9rem;
+  text-decoration: none;
+  display: block;
+  color: #3a3a71;
+}
+.contact-details a:nth-child(1) {
+  margin-bottom: 5px;
+}
+section {
+  padding-top: 30px;
+  min-height: 60vh;
+}
+section h2 {
+  text-align: center;
+}
+
+@media (min-width: 488px) {
+  h1 {
+    font-size: 4rem;
+  }
+  h2 {
+    font-size: 3.4rem;
+  }
+  p {
+    font-size: 1.6rem;
+  }
+
+  .header {
+    display: flex;
+    flex-direction: row;
+    vertical-align: middle;
+    align-content: space-between;
+    height: 80px;
+  }
+  .title {
+    display: flex;
+    align-items: center;
+  }
+  .title h1 {
+    margin: 0;
+  }
+  .title a {
+    margin: 0 30px;
+    display: flex;
+    align-items: center;
+    color: #1d2e7e;
+    text-decoration: none;
+  }
+  .contact-wrapper {
+    margin-left: auto;
+    display: flex;
+    width: 40vw;
+    align-items: center;
+    justify-content: center;
+  }
+  .contact-wrapper button {
+    padding: 10px 25px;
+    background-color: #3c61a0;
+    color: white;
+    font-size: 1.2rem;
+    cursor: pointer;
+    margin-left: 20px;
+  }
+  .footer {
+    background-color: #4e5d78;
+    color: white;
+    display: flex;
+    font-size: 1.4rem;
+    justify-content: space-around;
+    position: relative;
+  }
+  .footer .address li {
+    list-style: none;
+  }
+  .footer h4 {
+    text-align: center;
+  }
+  .footer a {
+    color: white;
+    text-decoration: none;
+  }
+  .contact-details a {
+    margin: 0;
+    font-size: 0.9rem;
+    text-decoration: none;
+    display: block;
+    color: #3a3a71;
+    font-size: 1.4rem;
+  }
+  .contact-details a:nth-child(1) {
+    margin-bottom: 5px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,299 @@
+<template>
+  <div>
+    <div class="hero-image">
+      <div class="hero-text">
+        <h1>Zinc Pressure Diecasting</h1>
+        <h2>from concept to completion</h2>
+      </div>
+    </div>
+    <section>
+      <h2>Our services</h2>
+      <div class="services-wrapper">
+        <a href="service" class="service-summary">
+          <div class="icon">
+            <i class="industrial-061-machine-press" />
+          </div>
+          <div class="service-title">
+            <h3>Component casting</h3>
+          </div>
+          <div class="summary">
+            <p>We will work with you to design your component to take advantage of the many benefits of zinc pressure diecasting.</p>
+          </div>
+          <div class="read-more">Read more</div>
+        </a>
+
+        <a href="service" class="service-summary">
+          <div class="icon">
+            <i class="marketing-024-laptop-1" />
+          </div>
+          <div class="service-title">
+            <h3>Tool design</h3>
+          </div>
+          <div class="summary">
+            <p>Tooling is designed in house using Solidworks CAD.</p>
+          </div>
+          <div class="read-more">Read more</div>
+        </a>
+
+        <a href="service" class="service-summary">
+          <div class="icon">
+            <i class="industrial-061-factory-2" />
+          </div>
+          <div class="service-title">
+            <h3>In house tool manufacture</h3>
+          </div>
+          <div class="summary">
+            <p>Complete with 3D CAD/CAM we produce all impression work in our own dedicated toolroom.</p>
+          </div>
+          <div class="read-more">Read more</div>
+        </a>
+
+        <a href="service" class="service-summary">
+          <div class="icon">
+            <i class="industrial-061-machine-press-2" />
+          </div>
+          <div class="service-title">
+            <h3>Diecast machining</h3>
+          </div>
+          <div class="summary">
+            <p>Using the latest diecasting technology we are able to reproduce quality castings in batches from 1000 to 5,000,000.</p>
+          </div>
+          <div class="read-more">Read more</div>
+        </a>
+
+        <a href="service" class="service-summary">
+          <div class="icon">
+            <i class="industrial-061-industrial-robot-15" />
+          </div>
+          <div class="service-title">
+            <h3>Secondary operations</h3>
+          </div>
+          <div class="summary">
+            <p>Tapping, drilling, CNC machining, barrelling and auto-shot blasting</p>
+          </div>
+          <div class="read-more">Read more</div>
+        </a>
+
+        <a href="service" class="service-summary">
+          <div class="icon">
+            <i class="ti-package" />
+          </div>
+          <div class="service-title">
+            <h3>Assembly</h3>
+          </div>
+          <div class="summary">
+            <p>Assembly of finished parts including powder coat, polishing and plating - all sourced locally</p>
+          </div>
+          <div class="read-more">Read more</div>
+        </a>
+      </div>
+    </section>
+    <section class="about">
+      <div>
+        <h2>About Jones & Wilkinson</h2>
+        <div class="about-wrapper">
+          <div class="about-cols">
+            <p>
+              Jones & Wilkinson Ltd, celebrating 50 years of engineering excellence, continue to have the knowledge, experience and facilities to help customers plan, design, and produce top quality tools and castings.
+            </p>
+            <p>
+              We currently operate from three units in Ezekiel lane, concentrating on Die Casting, Tool making, CNC machining and Assembly. The move into the third of these units, in late 2014, helped facilitate new processes to enhance current quality procedures.
+            </p>
+          </div>
+          <div class="about-cols img">
+            <img src="https://via.placeholder.com/500x300.png?text=map or photo">
+            <p class="caption">
+              Caption
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.about-wrapper {
+  display: flex;
+  flex-direction: column;
+
+}
+.about-cols {
+  /* flex-basis:50%; */
+  padding: 20px;
+}
+.about-cols p {
+  margin-bottom: 10px;
+}
+.about-cols.img {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+.about-cols.img img {
+  max-width: 300px;
+}
+section {
+  min-height: auto;
+}
+.hero-image {
+  background-image: linear-gradient(
+    rgba(26, 68, 140, 0.811),
+    rgba(26, 68, 140, 0.811)
+
+  ), url(~assets/img/cadcam.png);
+  height: 30vh;
+  width: 100vw;
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: cover;
+}
+.hero-text {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  height: 100%;
+  color: white;
+}
+
+.hero-image h2 {
+  font-size: 2rem;
+  font-style: italic;
+  margin: 0;
+}
+.hero-image h1 {
+  font-size: 2.4rem;
+  margin-top: 0;
+}
+.icon i {
+  font-size: 3rem;
+}
+
+.icon {
+  align-self: center;
+}
+
+.services-wrapper {
+  display: flex;
+  flex-wrap: wrap;
+}
+
+.service-summary {
+  flex: 40%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 20px 0px 0px 0px;
+  cursor: pointer;
+  border: solid 1px #d2d2d2;
+  margin: 5px;
+  border-radius: 15px;
+}
+
+.service-summary:hover {
+  background-color: rgba(77, 115, 189, 0.19);
+  border-radius: 15px;
+}
+.services-wrapper a {
+    text-decoration: none;
+    color: #000000;
+}
+.service-title {
+  align-self: center;
+  font-size: 1.1rem;
+  min-height: 30px;
+}
+.service-title h3 {
+  text-align: center;
+}
+.summary {
+  padding: 0 20px;
+  min-height: 50px;
+}
+.read-more {
+  align-self: center;
+  font-size: 0.8rem;
+  font-weight: 600;
+  text-decoration: underline;
+  margin: 10px 0;
+}
+.service-summary:hover .read-more {
+  color: #221d1d;
+}
+
+.about {
+  background: linear-gradient( rgba(255, 255, 255, 0.79), rgba(93, 117, 164, 0.67) ), url("~assets/img/plans.png");
+  background-size: cover;
+  margin-bottom: 70px;
+  padding-bottom: 50px;
+}
+.caption {
+  font-weight: 600;
+}
+
+@media (min-width: 480px) {
+  .hero-image {
+    height: 90vh;
+  }
+  .hero-image h2 {
+    font-size: 3.4rem;
+    margin-top: 20px;
+  }
+  .hero-image h1 {
+    font-size: 4.4rem;
+  }
+  .icon i {
+    font-size: 5rem;
+  }
+  .service-summary {
+    flex: 33%;
+    padding: 20px 0px;
+    border: none;
+    margin: 0;
+  }
+  .service-summary:hover {
+    background-color: rgba(77, 115, 189, 0.19);
+    border-radius: 15px;
+  }
+  .service-title {
+    font-size: 2.2rem;
+    min-height: 100px;
+  }
+  .summary {
+    min-height: 100px;
+  }
+  .read-more {
+    align-self: center;
+    font-size: 1.4rem;
+    font-weight: 600;
+    text-decoration: underline;
+    color: #ffffff;
+  }
+  .service-summary:hover .read-more {
+    display: block;
+    color: #000000;
+  }
+  .about {
+    margin-bottom: 0px;
+  }
+  .about-wrapper {
+    flex-direction: row;
+    padding: 30px 0px;
+  }
+  .about-cols {
+    flex-basis:50%;
+    padding: 20px;
+  }
+  .about-cols p {
+    margin-bottom: 10px;
+  }
+  .about-cols.img img {
+    max-width: 400px;
+  }
+}
+</style>
