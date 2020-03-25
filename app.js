e082c16d2ca91105a216dfea72c29e55c5dde967
Andy <andy@mediasuite.co.nz>
2020-03-25 21:30:47 +1300

    Fixed typo

--- app.js
+++ app.js
@@ -33,12 +33,4 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
 app.listen(3003);
